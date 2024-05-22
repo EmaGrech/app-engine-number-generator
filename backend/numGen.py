2f6de65635afb2b4e353a1259b38f40745283f40
EmaGrech <ema.grech.21@um.edu.mt>
2024-05-22 10:43:19 +0200

    Update numGen.py

--- backend/numGen.py
+++ backend/numGen.py
@@ -2,13 +2,14 @@ from flask import Flask, jsonify, request
 import random
 import sqlalchemy
 from google.cloud.sql.connector import Connector
-
+import logging
 
 app = Flask(__name__)
-
+logging.basicConfig(level=logging.INFO)
 
 # Configure database connection
 def connect():
+    logging.info("Establishing database connection...")
     connector = Connector()
     conn = connector.connect(
         "fifth-glazing-422823-j5:europe-west1:generator-history",
@@ -17,13 +18,14 @@ def connect():
         password="pass",
         db="Numbers_Generated"
     )
+    logging.info("Database connection established.")
     return conn
 
 
 #number generation
 @app.route('/generate', methods=['POST'])
 def generate():
-    
+    logging.info("Generating random numbers...")
     #connecting to database    
     conn = connect()
     cursor = conn.cursor()
@@ -43,12 +45,13 @@ def generate():
     cursor.close()
     conn.close()
 
+    logging.info("Random numbers generated and stored.")
     return jsonify({"status": "success", "instance": instance, "numbers": len(numbers)})
 
 #displaying results
 @app.route('/results', methods=['GET'])
 def results():
-    
+    logging.info("Fetching results...")
     #connecting to database 
     conn = connect()
     cursor = conn.cursor()
@@ -65,8 +68,10 @@ def results():
     output = {
         "instances": [{"instance": row[0], "count": row[1], "max": row[2], "min": row[3]} for row in results]
     }
+    logging.info("Results fetched successfully.")
     return jsonify(output)
 
 
 if __name__ == '__main__':
+    logging.info("Starting Flask application...")
     app.run(host='0.0.0.0', port=8080)
